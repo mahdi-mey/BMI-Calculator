dacff8555e1720cedfb616ad78ccd633b1c5488d
Mahdi <mahdimey17@gmail.com>
2023-10-02 18:21:20 +0330

    added function and eventlistener

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ let weightSpan = $.querySelector('#weight-val')
 let heightSpan = $.querySelector('#height-val')
 let bmiResult = $.querySelector('#result')
 let catagoryEl = $.querySelector('#category')
+
+function bmiCale(){
+    
+}
+
+userWeight.addEventListener('input', bmiCale)
+userHeight.addEventListener('input', bmiCale)
