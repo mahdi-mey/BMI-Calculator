a0cba7a9e4d2ae28b2752d262fa25bcc273ef0e4
Mahdi <mahdimey17@gmail.com>
2023-10-02 18:13:19 +0330

    selected dom elements

---
+++ script.js
@@ -0,0 +1,7 @@
+let $ = document
+let userWeight = $.querySelector('#weight')
+let userHeight = $.querySelector('#height')
+let weightSpan = $.querySelector('#weight-val')
+let heightSpan = $.querySelector('#height-val')
+let bmiResult = $.querySelector('#result')
+let catagoryEl = $.querySelector('#category')
