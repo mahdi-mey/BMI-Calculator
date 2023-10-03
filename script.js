ca740d60626b84f73ab5ebf606c51e943b370c9c
Mahdi <mahdimey17@gmail.com>
2023-10-03 11:38:22 +0330

    finished project

--- script.js
+++ script.js
@@ -7,7 +7,32 @@ let bmiResult = $.querySelector('#result')
 let catagoryEl = $.querySelector('#category')
 
 function bmiCale(){
-    
+    let weightValue = userWeight.value;
+    let heightValue = userHeight.value;
+
+    weightSpan.innerHTML = weightValue + ` kg`
+    heightSpan.innerHTML = heightValue +  ` cm`
+
+    let bmiValue = (weightValue / (Math.pow(heightValue/100, 2))).toFixed(1)
+    bmiResult.innerHTML = bmiValue
+
+    if(bmiValue < 18.5){
+        catagoryEl.innerHTML = 'Under Weight'
+        bmiResult.style.color = '#ffc44d'
+    }
+    else if(bmiValue >= 18.5 && bmiValue <= 24.9){
+        catagoryEl.innerHTML = 'Normal Weight'
+        bmiResult.style.color = '#0be881'
+    }
+    else if(bmiValue >= 25 && bmiValue < 24.9){
+        catagoryEl.innerHTML = 'Over Weight'
+        bmiResult.style.color = '#ff884d'
+    }
+    else{
+        catagoryEl.innerHTML = 'Obese'
+        bmiResult.style.color = '#ff5e4d'
+    }
+
 }
 
 userWeight.addEventListener('input', bmiCale)
